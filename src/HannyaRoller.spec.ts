47e847126c9440a6b605cafa35f1ddaedef27d1e
Masayuki Suzuki <m.suzuki.fp@gmail.com>
2019-03-16 16:24:25 -0700

    Added coverage

--- src/HannyaRoller.spec.ts
+++ src/HannyaRoller.spec.ts
@@ -1,4 +1,5 @@
-import HannyaRoller from './HannyaRoller'
+import HannyaRoller from './HannyaRoller';
+import text from './hannya-text';
 
 describe('HannyaRoller', () => {
   it('is OK', () => {
@@ -6,24 +7,24 @@ describe('HannyaRoller', () => {
     expect(2 + 2).toEqual(4)
   })
 
-  // /** @type HannyaRoller */
-  // let roller;
+  /** @type HannyaRoller */
+  let roller;
 
-  // /** @type HTMLDivElement */
-  // let el;
+  /** @type HTMLDivElement */
+  let el;
 
-  // beforeEach(() => {
-  //   el = window.document.createElement('div');
-  //   Object.defineProperty(el, 'clientWidth', { value: 800 });
-  //   Object.defineProperty(el, 'clientHeight', { value: 600 });
-  //   roller = new HannyaRoller({ el, text });
+  beforeEach(() => {
+    el = window.document.createElement('div');
+    Object.defineProperty(el, 'clientWidth', { value: 800 });
+    Object.defineProperty(el, 'clientHeight', { value: 600 });
+    roller = new HannyaRoller({ el, text });
 
-  //   roller.start();
-  // });
+    roller.start();
+  });
 
-  // it('sets up under the specified element', () => {
-  //   /** @type Element */
-  //   const elSpace = el.firstElementChild;
-  //   expect(elSpace.classList.contains('HannyaRoller-space')).toBeTruthy();
-  // });
+  it('sets up under the specified element', () => {
+    /** @type Element */
+    const elSpace = el.firstElementChild;
+    expect(elSpace.classList.contains('HannyaRoller-space')).toBeTruthy();
+  });
 })
